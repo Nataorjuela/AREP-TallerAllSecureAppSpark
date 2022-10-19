e26541f020e493c561addb1558dbd4d7929d6a3e
unknown <natalia.orjuela@labinfo.is.escuelaing.edu.co>
2022-10-19 17:02:40 -0500

    presentar certificado

--- src/main/java/escuelaing/edu/HelloWorld.java
+++ src/main/java/escuelaing/edu/HelloWorld.java
@@ -8,7 +8,7 @@ import static spark.Spark.*;
 public class HelloWorld {
     public static void main(String[] args) {
         //API: secure(keystoreFilePath, keystorePassword, truststoreFilePath,truststorePassword);
-        secure("keystores/ecikeypair.p12", "123456", null, null);
+        secure("keystore/ecikeystore.p12", "123456", null, null);
         port(5000);
         get("/hello", (req, res) -> "Hello World");
     }
