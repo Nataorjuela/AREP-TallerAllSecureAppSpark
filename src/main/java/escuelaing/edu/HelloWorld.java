023ad932852e2a939ec75f7543a4f036a8f5eb67
unknown <natalia.orjuela@labinfo.is.escuelaing.edu.co>
2022-10-19 16:55:47 -0500

    llaves publicas

--- src/main/java/escuelaing/edu/HelloWorld.java
+++ src/main/java/escuelaing/edu/HelloWorld.java
@@ -7,6 +7,7 @@ import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        port(5000);
         get("/hello", (req, res) -> "Hello World");
     }
 
